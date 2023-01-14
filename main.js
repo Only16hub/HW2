cabf68ac24d89c434cc57932ee0c30c1c26ddb03
Only16hub <simaoliynik29@gmail.com>
2023-01-13 18:14:19 -0800

    created repo and added 3 files into it\

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+	document.getElementById("button1").addEventListener("click", function () {
+		/*number1 = document.getElementById("inputNumber1").value;
+        number2 = document.getElementById("inputNumber2").value;
+        number3 = document.getElementById("inputNumber3").value;*/
+	});
+});
