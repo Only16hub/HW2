ada79710a57f188a50e2b5c8e985a940cce64499
StephenOren <orendain.stephen@gmail.com>
2023-01-15 19:56:42 -0800

    Added parseFloat in javascript

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 	document.getElementById("button1").addEventListener("click", function () {
-		number1 = document.getElementById("inputNumber1").value;
-        number2 = document.getElementById("inputNumber2").value;
-        number3 = document.getElementById("inputNumber3").value;
-
+		number1 = parseFloat(document.getElementById("inputNumber1").value);
+        number2 = parseFloat(document.getElementById("inputNumber2").value);
+        number3 = parseFloat(document.getElementById("inputNumber3").value);
 		average = (number1 + number2 + number3) / 3;
 		document.getElementById("result").innerHTML = average;
 	});
